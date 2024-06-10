b0ae742effe63409554790fbc5e7ca115f5f385a
Matt Gleich <git@mattglei.ch>
2024-06-09 23:14:10 -0400

    feat: show cache status
    Signed-off-by: Matt Gleich <git@mattglei.ch>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,35 @@
-import styles from './page.module.css';
-import Header from '@/ui/root/header/header';
+import styles from "./page.module.css";
+import Header from "@/ui/root/header/header";
+import Link from "next/link";
+import Cache from "@/ui/cache";
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <Header />
+      <div className={styles.introduction}>
+        <p>
+          Hey! I&apos;m Matt Gleich, a college student attending the{" "}
+          <Link href="https://rit.edu">
+            Rochester Institute of Technology (RIT)
+          </Link>
+          . I&apos;m studying computer science and in my free I really enjoy
+          cycling (gravel, road, & mountain bike) and photography. This website
+          pulls in a bunch of data automatically from my proxy cache api built
+          in rust (<Link href="https://github.com/gleich/lcp">gleich/lcp</Link>
+          )! Here is the status of each cache:
+        </p>
+        <div className={styles.caches}>
+          <Cache
+            name="Strava"
+            lastUpdate={new Date("2024-06-08T05:17:33.463127299Z")}
+          />
+          <Cache
+            name="Steam"
+            lastUpdate={new Date("2024-05-09T20:44:48.539286381Z")}
+          />
+        </div>
+      </div>
     </main>
   );
 }
---
+++ src/ui/cache.tsx
@@ -0,0 +1,43 @@
+import styles from "@/ui/cache.module.css";
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
+import { Inconsolata } from "next/font/google";
+
+const inconsolata = Inconsolata({ subsets: ["latin"] });
+
+export default function Cache({
+  name,
+  lastUpdate,
+}: {
+  name: string;
+  lastUpdate: Date;
+}) {
+  dayjs.extend(relativeTime);
+  const lastUpdateRelative = dayjs(lastUpdate).fromNow();
+  const lastUpdateExact = dayjs(lastUpdate).format("DD/MM/YYYY hh:MM A");
+  return (
+    <div className={`${styles.cache} ${inconsolata.className}`}>
+      <div className={styles.status}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          className={styles.icon}
+        >
+          <polyline points="20 6 9 17 4 12"></polyline>
+        </svg>
+        <p>{name} online</p>
+      </div>
+      <div className={styles.details}>
+        <p>Last updated {lastUpdateRelative}</p>
+        <p>{lastUpdateExact}</p>
+      </div>
+    </div>
+  );
+}
