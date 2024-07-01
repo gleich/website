3fe0330da063408adeb1e40c42e8f31afe8ff069
Matt Gleich <git@mattglei.ch>
2024-07-01 09:32:11 +0200

    feat: decrease font size and change time format for workouts
    Signed-off-by: Matt Gleich <git@mattglei.ch>

--- src/ui/root/workouts/time.tsx
+++ src/ui/root/workouts/time.tsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from 'react';
 import dayjs from 'dayjs';
 import duration from 'dayjs/plugin/duration';
 import timezone from 'dayjs/plugin/timezone';
+import advancedFormat from 'dayjs/plugin/advancedFormat';
 import utc from 'dayjs/plugin/utc';
 import styles from '@/ui/root/workouts/time.module.css';
 import { Inconsolata } from 'next/font/google';
@@ -13,6 +14,7 @@ const inconsolata = Inconsolata({ subsets: ['latin'] });
 dayjs.extend(duration);
 dayjs.extend(timezone);
 dayjs.extend(utc);
+dayjs.extend(advancedFormat);
 
 export default function Time({ date, tz }: { date: Date; tz: string }) {
   const [currentTime, setCurrentTime] = useState(dayjs());
@@ -49,7 +51,7 @@ export default function Time({ date, tz }: { date: Date; tz: string }) {
 
   return (
     <p className={`${styles.time} ${inconsolata.className}`}>
-      {dayjsDate.format('MM/DD/YYYY [at] h:mm A')} · {fromNow} ago
+      {dayjsDate.format(`MMMM Do [@] h:mm A`)} · {fromNow} ago
     </p>
   );
 }
