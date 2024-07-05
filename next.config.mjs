5bec392088d056d9e3efb6fae86b7c7075766c7e
Matt Gleich <git@mattglei.ch>
2024-07-05 12:54:46 -0400

    feat: remove millionjs
    Signed-off-by: Matt Gleich <git@mattglei.ch>

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,3 @@
-import million from 'million/compiler';
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
@@ -28,4 +26,4 @@ const nextConfig = {
   },
 };
 
-export default million.next(nextConfig);
+export default nextConfig;
