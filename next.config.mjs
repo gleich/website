4d1054baa3e0c29412329447ff81b426f4b794ba
Matt Gleich <git@mattglei.ch>
2024-07-11 14:43:01 -0400

    feat: socials redirects
    Signed-off-by: Matt Gleich <git@mattglei.ch>

--- next.config.mjs
+++ next.config.mjs
@@ -26,6 +26,28 @@ const nextConfig = {
       },
     ],
   },
+  redirects: async () => [
+    {
+      source: '/insta',
+      destination: 'https://www.instagram.com/mattglei.ch/',
+      permanent: true,
+    },
+    {
+      source: '/github',
+      destination: 'https://github.com/gleich',
+      permanent: true,
+    },
+    {
+      source: '/linkedin',
+      destination: 'https://www.linkedin.com/in/matt-gleich/',
+      permanent: true,
+    },
+    {
+      source: '/strava',
+      destination: 'https://www.strava.com/athletes/mattgleich',
+      permanent: true,
+    },
+  ],
 };
 
 export default million.next(nextConfig);
