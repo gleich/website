4a2e31146b0eb06a959cfe33175b3999177a2ccd
Matt Gleich <git@mattglei.ch>
2024-07-19 13:00:22 -0400

    feat: show today or yesterday for workouts
    Signed-off-by: Matt Gleich <git@mattglei.ch>

--- src/ui/root/workouts/time.tsx
+++ src/ui/root/workouts/time.tsx
@@ -28,9 +28,26 @@ export default function Time({ date, tz }: { date: Date; tz: string }) {
   }, []);
 
   const dayjsDate = dayjs(date).tz(tz.split(' ')[1]);
+  const yesterday = currentTime.subtract(1, 'day');
+  let dayOfWeek: string;
+  if (
+    currentTime.day() == dayjsDate.day() &&
+    currentTime.year() == dayjsDate.year() &&
+    currentTime.month() == dayjsDate.month()
+  ) {
+    dayOfWeek = 'Today';
+  } else if (
+    yesterday.day() == dayjsDate.day() &&
+    yesterday.year() == dayjsDate.year() &&
+    yesterday.month() == dayjsDate.month()
+  ) {
+    dayOfWeek = 'Yesterday';
+  } else {
+    dayOfWeek = dayjsDate.format('dddd');
+  }
   return (
     <p className={`${styles.time} ${inconsolata.className}`}>
-      {dayjsDate.format(`dddd, MMMM Do [at] h:mm A`)} [
+      {dayjsDate.format(`[${dayOfWeek}], MMMM Do [at] h:mm A`)} [
       {exactFromNow(dayjsDate, currentTime)}]
     </p>
   );
