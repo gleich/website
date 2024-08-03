efef9ef2514487a0225c9cb7e87fbebb87a3f8ba
Matt Gleich <git@mattglei.ch>
2024-08-03 12:07:03 -0400

    feat: update background color for favicon
    Signed-off-by: Matt Gleich <git@mattglei.ch>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,6 +22,7 @@ export const metadata: Metadata = {
     { rel: 'apple-touch-icon', url: '/apple-touch-icon.png', sizes: '180x180' },
     { rel: 'icon', url: '/favicon-32x32.png', sizes: '32x32' },
     { rel: 'icon', url: '/favicon-16x16.png', sizes: '16x16' },
+    { rel: 'mask-icon', url: '/safari-pinned-tab.svg', color: '#1C1E20' },
     { rel: 'icon', url: '/favicon.ico' },
   ],
   authors: [
