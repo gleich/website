af3920fcf542f91e2c1feaf236c519926cf2ec10
Matt Gleich <git@mattglei.ch>
2024-11-09 18:30:26 -0500

    feat: update site description
    Signed-off-by: Matt Gleich <git@mattglei.ch>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,7 @@ const inter = Inter({ subsets: ['latin'] });
 
 const title = 'Matt Gleich';
 const description =
-  'College student @ RIT, software engineer, cyclist, and photographer';
+  'College student @ RIT (Rochester Institute of Technology), software engineer, cyclist, and photographer';
 const ogImage = {
   url: 'https://mattglei.ch/opengraph.png',
   width: 1200,
