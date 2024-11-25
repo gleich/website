c2e1814e27f5492ef6f3bbda7042c92017f2c2ed
Matt Gleich <git@mattglei.ch>
2024-11-24 22:26:02 -0500

    fix: correct duplicate ref to scrolling text
    Signed-off-by: Matt Gleich <git@mattglei.ch>

--- src/ui/root/music/scrollingTitle.tsx
+++ src/ui/root/music/scrollingTitle.tsx
@@ -11,15 +11,19 @@ export default function ScrollingTitle({
   className?: string | undefined;
 }) {
   const containerRef = useRef<HTMLDivElement>(null);
-  const textRef = useRef<HTMLHeadingElement>(null);
+  const mainTextRef = useRef<HTMLHeadingElement>(null);
   const [isOverflowing, setIsOverflowing] = useState(false);
 
   useEffect(() => {
     const container = containerRef.current;
-    const text = textRef.current;
+    const text = mainTextRef.current;
     if (container && text) {
       const checkOverflow = () => {
-        setIsOverflowing(text.scrollWidth > container.clientWidth);
+        if (container && mainTextRef.current) {
+          setIsOverflowing(
+            mainTextRef.current.scrollWidth > container.clientWidth,
+          );
+        }
       };
       checkOverflow();
       window.addEventListener('resize', checkOverflow);
@@ -33,14 +37,13 @@ export default function ScrollingTitle({
       className={`${styles.container} ${isOverflowing ? '' : styles.notOverflow}`}
     >
       <div
-        ref={textRef}
+        ref={mainTextRef}
         className={`${styles.text} ${className} ${isOverflowing ? styles.scroll : ''}`}
       >
         {text}
       </div>
       {isOverflowing ? (
         <div
-          ref={textRef}
           className={`${styles.text} ${className} ${isOverflowing ? styles.scroll : ''}`}
         >
           {text}
