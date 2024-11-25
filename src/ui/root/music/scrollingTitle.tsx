8db3a6a56e14a26375caa6b44020a3d73249da41
Matt Gleich <git@mattglei.ch>
2024-11-25 18:24:47 -0500

    feat: have marquee delay scroll by 2s instead of 4s
    Signed-off-by: Matt Gleich <git@mattglei.ch>

--- src/ui/root/music/scrollingTitle.tsx
+++ src/ui/root/music/scrollingTitle.tsx
@@ -39,7 +39,7 @@ export default function ScrollingTitle({
         <Marquee
           gradient={false}
           speed={40}
-          delay={4}
+          delay={2}
           className={`${styles.marquee} ${className}`}
         >
           <span className={styles.marqueeText}>{text}</span>
