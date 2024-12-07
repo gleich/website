3c69db09c13cc25d8ff4738188b33a616e035d9a
Matt Gleich <git@mattglei.ch>
2024-12-07 15:53:16 -0500

    feat: add week to time output
    Signed-off-by: Matt Gleich <git@mattglei.ch>

--- src/lib/time.ts
+++ src/lib/time.ts
@@ -5,17 +5,26 @@ dayjs.extend(duration);
 
 export function exactFromNow(date: Dayjs, currentTime: Dayjs): string {
   const yearsDiff = Math.abs(date.diff(currentTime, 'year'));
-  const monthsDiff = Math.abs(date.diff(currentTime, 'month')) % 12;
-  const daysDiff = Math.abs(date.diff(currentTime, 'day')) % 30;
+  const totalMonthsDiff = Math.abs(date.diff(currentTime, 'month'));
+  const monthsDiff = totalMonthsDiff % 12;
+
+  const totalDaysDiff = Math.abs(date.diff(currentTime, 'day'));
+
+  const weeksDiff = totalDaysDiff >= 7 ? Math.floor(totalDaysDiff / 7) : 0;
+  const daysDiff = weeksDiff > 0 ? totalDaysDiff % 7 : totalDaysDiff % 30;
+
   const hoursDiff = Math.abs(date.diff(currentTime, 'hour')) % 24;
   const minutesDiff = Math.abs(date.diff(currentTime, 'minute')) % 60;
   const secondsDiff = Math.abs(date.diff(currentTime, 'second')) % 60;
+
   let fromNow: string;
 
   if (yearsDiff > 0) {
     fromNow = `${yearsDiff} ${yearsDiff === 1 ? 'year' : 'years'} & ${monthsDiff} ${monthsDiff === 1 ? 'month' : 'months'}`;
   } else if (monthsDiff > 0) {
     fromNow = `${monthsDiff} ${monthsDiff === 1 ? 'month' : 'months'} & ${daysDiff} ${daysDiff === 1 ? 'day' : 'days'}`;
+  } else if (weeksDiff > 0) {
+    fromNow = `${weeksDiff} ${weeksDiff === 1 ? 'week' : 'weeks'} & ${daysDiff} ${daysDiff === 1 ? 'day' : 'days'}`;
   } else if (daysDiff > 0) {
     fromNow = `${daysDiff} ${daysDiff === 1 ? 'day' : 'days'} & ${hoursDiff}hr`;
   } else if (hoursDiff > 0) {
