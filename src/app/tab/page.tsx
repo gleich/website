eac8055b9b5efcdbd8483fac0974d9375efa0091
Matt Gleich <git@mattglei.ch>
2024-12-08 00:05:07 -0500

    feat: add copyright to new tab page
    Signed-off-by: Matt Gleich <git@mattglei.ch>

--- src/app/tab/page.tsx
+++ src/app/tab/page.tsx
@@ -2,6 +2,7 @@ import styles from '@/app/tab/page.module.css';
 import Logo from '@/ui/root/header/logo';
 import Time from './time';
 import { Metadata } from 'next';
+import Copyright from '@/ui/root/copyright';
 
 export const metadata: Metadata = {
   title: 'mattglei.ch',
@@ -11,9 +12,9 @@ export default function TabPage() {
   return (
     <main className={styles.main}>
       <Logo className={styles.logo} />
-      <div className={styles.text}>
-        <Time />
-        <p className={styles.siteName}>mattglei.ch</p>
+      <Time />
+      <div className={styles.copyright}>
+        <Copyright />
       </div>
     </main>
   );
