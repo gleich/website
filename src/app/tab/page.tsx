570d10287b82915e1259e3b929a1b472964be728
Matt Gleich <git@mattglei.ch>
2024-12-07 22:43:29 -0500

    feat: change site title for tab page
    Signed-off-by: Matt Gleich <git@mattglei.ch>

--- src/app/tab/page.tsx
+++ src/app/tab/page.tsx
@@ -1,6 +1,11 @@
 import styles from '@/app/tab/page.module.css';
 import Logo from '@/ui/root/header/logo';
 import Time from './time';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'mattglei.ch',
+};
 
 export default function TabPage() {
   return (
