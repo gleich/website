d0f777ad001b3a771e1796101a2db45d5feb70ee
Matt Gleich <git@mattglei.ch>
2025-03-03 23:56:30 -0500

    feat: try to render dayjs date with utc first
    Signed-off-by: Matt Gleich <git@mattglei.ch>

--- src/ui/root/workouts/time.tsx
+++ src/ui/root/workouts/time.tsx
@@ -27,7 +27,7 @@ export default function Time({ date, tz }: { date: Date; tz: string }) {
     return () => clearInterval(interval);
   }, []);
 
-  const dayjsDate = dayjs(date).tz(tz.split(' ')[1]);
+  const dayjsDate = dayjs(date).utc(true).tz(tz.split(' ')[1]);
   const yesterday = currentTime.subtract(1, 'day');
   let dayOfWeek: string;
   if (
